e7b517fe1f45985e767ef9ede1617c5a2fd1df08
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-06-27 17:11:21 +0300

    Fix bug in menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
 	var touch 	= $('#touch-menu');
 	var menu 	= $('.menu');
 
+
 	$(touch).on('click', function(e) {
 		e.preventDefault();
-		menu.slideToggle();
+
+		menu.slideToggle();		
 	});
+
+
+	
+	$('a','.menu').click(function() {
+		var w = $(window).width();
+		if(w < 790) {
+			$('.menu').toggle(0);
+		}
+	});
+		
+	  
 
 	$(window).resize(function(){
 		var w = $(window).width();
