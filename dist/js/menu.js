f1bee812c583c5b6e35d1702a7256b99903f71f3
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-08-28 12:08:44 +0300

    Build project

---
+++ dist/js/menu.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	var touch 	= $('#touch-menu');
+	var menu 	= $('.menu');
+
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+
+		menu.slideToggle();		
+	});
+
+
+	
+	$('a','.menu').click(function() {
+		var w = $(window).width();
+		if(w < 790) {
+			$('.menu').toggle(0);
+		}
+	});
+		
+	  
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
+});
